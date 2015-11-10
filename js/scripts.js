37c41b4d1fec46bac3ad9d19577ece22f1ac2e28
Vince Elizaga <vncelizaga@gmail.com>
2015-11-10 15:09:33 +0800

    add black overlay on show sidemenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,45 @@
 
 $(document).ready(function(){/* activate scrollspy menu */
-$('body').scrollspy({
-  target: '#navbar-collapsible',
-  offset: 50
-});
+  $('body').scrollspy({
+    target: '#navbar-collapsible',
+    offset: 50
+  });
 
-/* smooth scrolling sections */
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 50
-        }, 1000);
-        return false;
+  /* smooth scrolling sections */
+  $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 50
+          }, 1000);
+          return false;
+        }
       }
+  });
+  $('#blackOverlay').hide();
+
+  $('#myNavmenu').on('hidden.bs.offcanvas', function (e) {
+    $('#blackOverlay').hide();
+  });
+
+  $('#myNavmenu').on('show.bs.offcanvas', function (e) {
+    $('#blackOverlay').show();
+  });
+
+  var targetOffset = $("#section1").offset().top;
+  $('#scrolled-nav-bar').hide();
+  $('#initial-nav-bar').show();
+
+  var $w = $(window).scroll(function(){
+    if ( $w.scrollTop() > 200 ) {
+      $('#scrolled-nav-bar').fadeIn( 'fast' );
+      $('#initial-nav-bar').hide();
+    } else {
+      $('#scrolled-nav-bar').hide();
+      $('#initial-nav-bar').fadeIn( 'fast' );
     }
-});
-
-var targetOffset = $("#section1").offset().top;
-console.log(targetOffset);
-$('#scrolled-nav-bar').hide();
-$('#initial-nav-bar').show();
-
-var $w = $(window).scroll(function(){
-  if ( $w.scrollTop() > 200 ) {
-    $('#scrolled-nav-bar').fadeIn( 'fast' );
-    $('#initial-nav-bar').hide();
-    console.log('here');
-  } else {
-    $('#scrolled-nav-bar').hide();
-    $('#initial-nav-bar').fadeIn( 'fast' );
-    console.log('there');
-  }
-});
+  });
 
 });

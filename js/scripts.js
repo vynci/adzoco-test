3d4a179a9a27b4e3f3d3bf147fdb0f11d026ff5a
Vince Elizaga <vncelizaga@gmail.com>
2015-11-12 16:10:16 +0800

    added test animations

--- js/scripts.js
+++ js/scripts.js
@@ -32,13 +32,63 @@ $(document).ready(function(){/* activate scrollspy menu */
   $('#scrolled-nav-bar').hide();
   $('#initial-nav-bar').show();
 
+  $('#sec1-window').hide();
+  $('#sec1-plus').hide();
+  $('#sec1-adv').hide();
+  $('#sec1-equal').hide();
+  $('#sec1-money').hide();
+
+  $('#sec2-img1').hide();
+  $('#sec2-img2').hide();
+  $('#sec2-img3').hide();
+  $('#sec2-img4').hide();
+
+  $('#sec3-img1').hide();
+  $('#sec3-img2').hide();
+  $('#sec3-img3').hide();
+  $('#sec3-img4').hide();
+
+  $('#sec1-window').fadeIn('slow', function(){
+    $('#sec1-plus').fadeIn('slow', function(){
+      $('#sec1-adv').fadeIn('slow', function(){
+        $('#sec1-equal').fadeIn('slow', function(){
+          $('#sec1-money').fadeIn('slow', function(){
+
+          });
+        });
+      });
+    });
+  });
+
   var $w = $(window).scroll(function(){
-    if ( $w.scrollTop() > 200 ) {
+    if ( $w.scrollTop() > 150 ) {
       $('#scrolled-nav-bar').slideDown( 'fast' );
       $('#initial-nav-bar').slideUp('fast');
     } else {
       $('#scrolled-nav-bar').slideUp('fast');
       $('#initial-nav-bar').slideDown( 'fast' );
+    }
+
+    if ( $w.scrollTop() >= ( $('#section2').position().top - 100 ) ) {
+      $('#sec2-img1').fadeIn('slow', function(){
+        $('#sec2-img2').fadeIn('slow', function(){
+          $('#sec2-img3').fadeIn('slow', function(){
+            $('#sec2-img4').fadeIn('slow', function(){
+            });
+          });
+        });
+      });
+    }
+
+    if ( $w.scrollTop() >= ( $('#section3').position().top - 100 ) ) {
+      $('#sec3-img1').fadeIn('slow', function(){
+        $('#sec3-img2').fadeIn('slow', function(){
+          $('#sec3-img3').fadeIn('slow', function(){
+            $('#sec3-img4').fadeIn('slow', function(){
+            });
+          });
+        });
+      });
     }
   });
 
